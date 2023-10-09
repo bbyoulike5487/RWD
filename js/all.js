a12471b52eb40ed87667f733c64855af46c9d553
bbyoulike5487 <bbyoulike5487@gmail.com>
2023-10-09 18:31:20 +0800

    23.10.09

--- js/all.js
+++ js/all.js
@@ -8,3 +8,27 @@ $(document).ready(function () {
       }
     });  
   });
+
+  const login = document.querySelector('.login');
+  const register = document.querySelector('.register');
+  const registerBtn = document.querySelector('.registerBtn');
+  const loginBtn = document.querySelector('.loginBtn');
+  const registerBtn2 = document.querySelector('.registerBtn2');
+
+  //到註冊頁面
+  registerBtn.addEventListener('click',() => {
+    register.classList.remove("none");
+    login.classList.add("none");
+  })
+  //到登入頁面
+  registerBtn2.addEventListener('click',() => {
+    setTimeout(() => {
+      register.classList.add("none");
+      login.classList.remove("none");
+    }, 2000)
+    alert('註冊成功！');
+  })
+  //登入成功
+  loginBtn.addEventListener('click',() => {
+    alert('登入成功！');
+  })
